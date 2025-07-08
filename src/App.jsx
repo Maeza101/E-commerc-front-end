f43397612e60fa786fe8059c3abd8591147c0b16
Maeza101 <makheler@caa.co.za>
2025-07-08 19:27:15 +0200

    Initial commit front-end

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Header from './static/Header';
+import Footer from './static/footer';
+import Home from './pages/home';
+import Product from './pages/Product';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+import OrderReview from './pages/OrderReview';
+
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/Products' element={<Product />} />
+          <Route path='/products/detail' element={<ProductDetails />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/cart/OrderReview' element={<OrderReview />} />
+
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+
+
+
+
+
